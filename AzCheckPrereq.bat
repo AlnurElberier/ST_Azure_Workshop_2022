eb25e441b1040f543b4cdd7440fd27ced706af2f
AlnurElberier <a.elberier@gmail.com>
2022-09-20 10:53:29 -0700

    post dry run updates

--- AzCheckPrereq.bat
+++ AzCheckPrereq.bat
@@ -8,43 +8,50 @@ if exist %stm32programmercli% (
     echo.
 ) else (
     echo ERR: STM32CubeProgrammer Not Installed 
+    echo You will be redierected to download STM32CubeProgrammer
+    echo Please Install STM32CubeProgrammer and run the script again
+    pause 5
     start "" https://www.st.com/en/development-tools/stm32cubeprog.html#get-software
+    goto :err
 )
 
 
 python --version 2>NUL
 if errorlevel 1 (
     echo ERR: Python Not Installed 
+    echo You will be redierected to download Python
+    echo Please Install Python and run the script again
+    pause 5
     start "" https://www.python.org/downloads/
+    goto :err
 ) else (
     echo Python Successfully Installed
     echo.
 )
 
 
-@REM pip --version 2>NUL
-@REM if errorlevel 1 (
-@REM     echo ERR: pip Not Installed 
-@REM     start "" https://www.geeksforgeeks.org/how-to-install-pip-on-windows/
-@REM ) else (
-@REM     echo pip Successfully Installed
-@REM     echo.
-@REM )
+pip --version 2>NUL
+if errorlevel 1 (
+    echo ERR: pip Not Installed 
+    echo You will be redierected to Download Pip
+    echo Please Install pip and run the script again
+    pause 5
+    start "" https://www.geeksforgeeks.org/how-to-install-pip-on-windows/
+    goto :err
+) else (
+    echo pip Successfully Installed
+    echo.
+    call pip install pyserial
+    
+)
 
-@REM git --version 2>NUL
-@REM if errorlevel 1 (
-@REM     echo ERR: Git Not Installed 
-@REM     start "" https://git-scm.com/download/win
-@REM ) else (
-@REM     echo Git Successfully Installed
-@REM     echo.
-@REM )
-
-call pip install pyserial
 
 call az --version 2>NUL
 if errorlevel 1 (
     echo ERR: AZ CLI Not Installed 
+    echo You will be redierected to download AZCLI
+    echo Please Install AZCLI and run the script again
+    pause 5
     start "" https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-windows?tabs=azure-cli
 ) else (
     echo AZ CLI Successfully Installed
@@ -52,12 +59,27 @@ if errorlevel 1 (
 )
 
 
-call az extension add --name azure-iot
+call az extension add --name azure-iot 
 call az extension update --name azure-iot
-call az login
 call az extension add --name account
-call az account list
-call az account tenant list
+call az extension update --name account
 
+call az login --allow-no-subscription
+call az account tenant list > tenant.txt
 
+for %%i in (tenant.txt) do (
+    if %%~z%i LSS 100 (
+        echo. 
+        echo You azure account is not associated with a tenant
+        echo Send an email to alnur.elberier@st.com to request to be added to a tenant
+        echo Check your email to accept the tenant invitation. 
+        echo Run this script again after joining the tenant. 
+        goto :err
+    )
+)
+
+echo.
+echo Requirement Check Success
+
+:err
 pause
