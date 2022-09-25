febf6ae916f41a52c177604f302e667caffa69db
SlimJallouli <slim.jallouli@st.com>
2022-09-25 13:27:41 -0700

    add links as parameters. download tools and install from the tools directory. remove get as get tenant

--- AzCheckPrereq.bat
+++ AzCheckPrereq.bat
@@ -4,6 +4,11 @@ set stm32programmercli="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeP
 set STM32CubeExpansion_Cloud_AZURE="C:\STM32CubeExpansion_Cloud_AZURE_V2.0.1"
 set rerun=1==0
 
+set DOWNLOAD_LINK_STM32_CUBE_PROG="https://negzxq.sn.files.1drv.com/y4m5-WjjhqkTLyb2AmUUahxlP5NNRJuI6eCQ8-8Q7S2GvJLDEIPqoQ-SenyXrm1iUschFYDiZv1Gky_qNsdafs94wi00s-GMwCnCe3ANApfh96yVoWdr3vEvDIV6DQkI_AHInvaxUW_a_8Zgs08ESSwICrg7tjBWz4tEtS579T7USqU2XqL4_U12dt8rffwcZckdz0N7xWHN4FiQDviYizGog"
+set DOWNLOAD_LINK_PYTHON="https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe"
+set DOWNLOAD_LINK_AZCLI=https://azcliprod.blob.core.windows.net/msi/azure-cli-2.40.0.msi
+set DOWNLOAD_LINK_X_CUBE_AZURE="https://nejo0q.sn.files.1drv.com/y4mYF8oM2L4xrPIeHzInK8O1fqyT4P3rXB6Iy3AmDBy4s5iVyANS3IpfjkzpSSLY5N-wdPWUSZ5bCf8F3yif8HCs4dnmit407VqkuLmB4x2dva5oyrrsfKgFucPMHJS7-GUzThaDW-RH1TthaUYhA_3bqvt4JgJ27wcGXaODeBQRuM66jZDXeM_enQXNLT-6C-gx79YFPU1I6XOe8JHIA0XTQ"
+set DOWNLOAD_LINK_GET_PIP="https://bootstrap.pypa.io/get-pip.py"
 echo. 
 
 if exist %stm32programmercli% ( 
@@ -13,9 +18,9 @@ if exist %stm32programmercli% (
 ) else (
     echo.
     echo Installing STM32CubeProgrammer
-    curl "https://public.sn.files.1drv.com/y4mC3dcNQf9JVHYkFcuZBk0keyYWYY_RlV07C99jUaHlO9-hOd8YRGrtPwsDaMa20EO35GwUeFC9Lw8q-2Ifw8yv1fFd-pYUGeS94MiUue7W7dnWMVxBBTg-iI8MR397vRsVBYuzdo_hDTCBOAH4Xhs4LViTCTBNHMPdfLGypC9Jd3frFrvBT9C5H_o77cbFKkvEe4E62pIueKJ7aWXac6K5CGNhabF9ycJBOQJms1gZtM" -o "en.stm32cubeprg-win64_v2-11-0.zip"
-    call powershell -command "Expand-Archive en.stm32cubeprg-win64_v2-11-0.zip"
-    call start .\en.stm32cubeprg-win64_v2-11-0\SetupSTM32CubeProgrammer_win64.exe
+    curl %DOWNLOAD_LINK_STM32_CUBE_PROG% -o ".\tools\en.stm32cubeprg-win64_v2-11-0.zip"
+    call powershell -command "Expand-Archive .\tools\en.stm32cubeprg-win64_v2-11-0.zip .\tools\en.stm32cubeprg-win64_v2-11-0"
+    call .\tools\en.stm32cubeprg-win64_v2-11-0\SetupSTM32CubeProgrammer_win64.exe
     echo.
 )
 
@@ -23,7 +28,8 @@ python --version 2>NUL
 if errorlevel 1 (
     echo.
     echo Installing Python
-    call powershell  .\scripts\install_python.ps1
+    curl  %DOWNLOAD_LINK_PYTHON% -o ".\tools\python-3.10.7-amd64.exe"
+    call  .\tools\python-3.10.7-amd64.exe /passive InstallAllUsers=1 PrependPath=1 Include_test=0 
     set rerun=1==1
     echo.
 ) else (
@@ -38,7 +44,8 @@ call az --version 2>NUL
 if errorlevel 1 (
     echo.
     echo Installing AZCLI
-    call powershell  .\scripts\install_azcli.ps1
+    curl %DOWNLOAD_LINK_AZCLI% -o ".\tools\azure-cli-2.40.0.msi"
+    call .\tools\azure-cli-2.40.0.msi
     set rerun=1==1
     echo.
 ) else (
@@ -54,9 +61,9 @@ if exist %STM32CubeExpansion_Cloud_AZURE% (
     echo.
 ) else (
     echo Downloading X-CUBE-AZURE
-    curl "https://public.sn.files.1drv.com/y4my4D2H2XthqPvH0Co2SuTHQfSaKll2lOR_TI_F68Rl_pRQNYJmtaMnUimHSZtPmKDLwHUnotFudCMg3YOxA5_oQcYyNF1_4R983AvTCv9BlqvQnZkiAUKhi9nZa7FiaCcnuL3ZzGQZKM4th5rgor86YcCPCaafSYLdLK-RX6GalLXhNX81u4zGTUqzFKav5kq0_s9MTF8QuuJywebLN3StBrdqKlz3PBeFeb1wEA0yYE" -o "en.x-cube-azure_v2-0-1.zip"
+    curl %DOWNLOAD_LINK_X_CUBE_AZURE% -o ".\tools\en.x-cube-azure_v2-0-1.zip"
     echo Extracting X-CUBE-AZURE
-    powershell -command "Expand-Archive en.x-cube-azure_v2-0-1.zip C:\."
+    powershell -command "Expand-Archive .\tools\en.x-cube-azure_v2-0-1.zip C:\."
 )
 
 
@@ -69,7 +76,7 @@ if %rerun% (
         echo.
         echo ERR: pip Not Installed 
         echo Pip will now be installed
-        curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+        curl %DOWNLOAD_LINK_GET_PIP" -o get-pip.py
         python get-pip.py
         echo.
     ) else (
@@ -100,19 +107,6 @@ if %rerun% (
     echo Redirecting to a browser window to log in to Azure Cli
     echo Please return to the script after logging in
     call az login --allow-no-subscription
-    call az account tenant list > tenant.txt
-
-    for %%i in (tenant.txt) do (
-        if %%~z%i LSS 100 (
-            echo. 
-            echo.
-            echo You azure account is not associated with a tenant
-            echo Send an email to alnur.elberier@st.com to request to be added to a tenant
-            echo Check your email to accept the tenant invitation
-            echo Run this script again after joining the tenant
-        )
-    )
-
 
     call python .\scripts\configureJson.py
 )
